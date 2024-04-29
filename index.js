7dfaea8b2a6bfb1c971457e8d608c3c478bfa88d
Alexey <alexeyf08@@gmail.com>
2024-04-29 18:02:20 +0700

    Добавлен обработчик ошибок

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Bot } = require('grammy');
+const { Bot, GrammyError, HttpError } = require('grammy');
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
@@ -10,5 +10,19 @@ bot.command('start', async (ctx) => {
 bot.on('message', async (ctx) => {
   await ctx.reply('Привет мир');
 });
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+  });
 
 bot.start();
