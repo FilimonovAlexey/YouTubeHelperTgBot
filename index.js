bbb842669dceea38091dc872e5b09bc1586021fb
Alexey <alexeyf08@@gmail.com>
2024-05-01 20:01:24 +0700

    Создан единный обработчик для событий

--- index.js
+++ index.js
@@ -42,57 +42,129 @@ bot.hears('Социальные сети', async (ctx) => {
   });
 });
 
-bot.on('callback_query:data', async (ctx) => {
-  const socialNetwork = ctx.callbackQuery.data;
-
-  // Определяем URL для выбранной социальной сети
-  let socialUrl;
-  switch (socialNetwork) {
-    case 'YouTube':
-      socialUrl = 'https://www.youtube.com/@tehno.maniak';
-      break;
-    case 'Telegram':
-      socialUrl = 'https://t.me/tehnomaniak07';
-      break;
-    case 'Vk':
-      socialUrl = 'https://vk.com/public212223166';
-      break;
-    case 'ДЗЕН':
-      socialUrl = 'https://dzen.ru/filimonov-blog.ru';
-      break;
-    case 'TikTok':
-        socialUrl = 'https://dzen.ru/filimonov-blog.ru';
-      break;
-    case 'X':
-        socialUrl = 'https://dzen.ru/filimonov-blog.ru';
-    break;
-    case 'Instagram':
-      socialUrl = 'https://dzen.ru/filimonov-blog.ru';
-    break;
-    default:
-      socialUrl = '';
-  }
-
-  // Отправляем ссылку на социальную сеть
-  if (socialUrl) {
-    await ctx.reply(`Вот ваша ссылка на ${socialNetwork}: ${socialUrl}`);
-  } else {
-    await ctx.reply('Извините, что-то пошло не так.');
-  }
-});
-
 bot.hears('Промокоды и скидки', async (ctx) => {
   // Создаем инлайновую клавиатуру
   const promoKeyboard = new InlineKeyboard()
-    .text('Хостинг сервера', 'Хостинг сервера')
+    .text('Хостинг сервера', 'hosting')
     .row()
-    .text('Яндекс практикум', 'Яндекс практикум')
+    .text('Яндекс практикум', 'yandex')
 
   // Отправляем клавиатуру в ответе на сообщение
   await ctx.reply('Выберите категорию:', {
     reply_markup: promoKeyboard,
   });
 });
+
+//Единный обработчик для двух событий
+bot.on('callback_query', async (ctx) => {
+  const query = ctx.callbackQuery.data;
+  let message = '';
+
+  // Проверяем контекст запроса
+  if (query === 'hosting' || query === 'yandex') {
+    // Отправляем сообщение для категории "Промокоды и скидки"
+    switch (query) {
+      case 'hosting':
+        message = 'Вот ссылка на хостинг сервера: https://example.com/hosting';
+        break;
+      case 'yandex':
+        message = 'Вот ссылка на Яндекс практикум: https://example.com/yandex';
+        break;
+      default:
+        message = 'Извините, что-то пошло не так.';
+    }
+  } else {
+    // Обработка других типов запросов, например, социальных сетей
+    // Определяем URL для выбранной социальной сети
+    switch (query) {
+      case 'YouTube':
+        message = 'Вот ссылка на YouTube: https://www.youtube.com/@tehno.maniak';
+        break;
+      case 'Telegram':
+        message = 'Вот ссылка на Telegram: https://t.me/tehnomaniak07';
+        break;
+      case 'Vk':
+        message = 'Вот ссылка на ВКонтакте: https://vk.com/public212223166';
+        break;
+      case 'ДЗЕН':
+        message = 'Вот ссылка на Дзен: https://dzen.ru/filimonov-blog.ru';
+        break;
+      case 'TikTok':
+        message = 'Вот ссылка на TikTok: https://www.tiktok.com/';
+        break;
+      case 'X':
+        message = 'Вот ссылка на X: https://example.com/x';
+        break;
+      case 'Instagram':
+        message = 'Вот ссылка на Instagram: https://www.instagram.com/';
+        break;
+      default:
+        message = 'Извините, что-то пошло не так.';
+    }
+  }
+
+  await ctx.reply(message);
+});
+
+
+// bot.on('callback_query:data', async (ctx) => {
+//   const socialNetwork = ctx.callbackQuery.data;
+
+//   // Определяем URL для выбранной социальной сети
+//   let socialUrl;
+//   switch (socialNetwork) {
+//     case 'YouTube':
+//       socialUrl = 'https://www.youtube.com/@tehno.maniak';
+//       break;
+//     case 'Telegram':
+//       socialUrl = 'https://t.me/tehnomaniak07';
+//       break;
+//     case 'Vk':
+//       socialUrl = 'https://vk.com/public212223166';
+//       break;
+//     case 'ДЗЕН':
+//       socialUrl = 'https://dzen.ru/filimonov-blog.ru';
+//       break;
+//     case 'TikTok':
+//         socialUrl = 'https://dzen.ru/filimonov-blog.ru';
+//       break;
+//     case 'X':
+//         socialUrl = 'https://dzen.ru/filimonov-blog.ru';
+//     break;
+//     case 'Instagram':
+//       socialUrl = 'https://dzen.ru/filimonov-blog.ru';
+//     break;
+//     default:
+//       socialUrl = '';
+//   }
+
+//   // Отправляем ссылку на социальную сеть
+//   if (socialUrl) {
+//     await ctx.reply(`Вот ваша ссылка на ${socialNetwork}: ${socialUrl}`);
+//   } else {
+//     await ctx.reply('Извините, что-то пошло не так.');
+//   }
+// });
+
+// bot.on('callback_query', async (ctx) => {
+//   const query = ctx.callbackQuery.data;
+//   let message = '';
+
+//   switch (query) {
+//     case 'hosting':
+//       message = 'Вот ссылка на хостинг сервера: https://example.com/hosting';
+//       break;
+//     case 'yandex':
+//       message = 'Вот ссылка на Яндекс практикум: https://example.com/yandex';
+//       break;
+//     default:
+//       message = 'Извините, что-то пошло не так.';
+//   }
+
+//   await ctx.reply(message);
+// });
+
+
 
 bot.hears('Предложка', async (ctx) => {
   await ctx.reply('Опишите ваше предложение или сообщение, которое вы хотели бы отправить автору бота.');
