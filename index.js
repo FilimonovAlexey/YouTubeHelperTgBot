2afd95e1b42f9be5c07deaf4c17955e6b7344d73
Alexey <alexeyf08@@gmail.com>
2024-05-02 09:07:09 +0700

    Перенос вспомогательных функций в отдельный файл

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Bot, GrammyError, HttpError, Keyboard, InlineKeyboard } = require('grammy');
 const fs = require('fs');
-const { updateUserData, isAdmin } = require('./utils/helpers');
+const { updateUserData, isAdmin, createKeyboard } = require('./utils/helpers');
 const { socialNetworks, promoCodes } = require('./utils/buttons');
 
 const bot = new Bot(process.env.BOT_API_KEY);
@@ -51,36 +51,6 @@ bot.command('admin', async (ctx) => {
     await ctx.reply('У вас нет прав администратора!');
   }
 });
-
-// Функция для создания клавиатуры с кнопками и кнопкой "Назад"
-function createKeyboard(buttons) {
-  const keyboard = new Keyboard();
-  const buttonCount = buttons.length;
-  
-  // Вычисляем количество кнопок в каждой колонке
-  const buttonsPerColumn = Math.ceil(buttonCount / 2);
-  
-  for (let i = 0; i < buttonsPerColumn; i++) {
-    // Добавляем кнопку в первую колонку
-    const index1 = i;
-    keyboard.text(buttons[index1].name);
-    
-    // Проверяем, есть ли кнопка для второй колонки
-    const index2 = i + buttonsPerColumn;
-    if (index2 < buttonCount) {
-      // Добавляем кнопку во вторую колонку
-      keyboard.text(buttons[index2].name);
-    }
-    
-    // Переходим на следующую строку
-    keyboard.row();
-  }
-  
-  // Добавляем кнопку "Назад"
-  keyboard.text('Назад');
-  
-  return keyboard;
-}
 
 // Функция для обработки нажатий на кнопки
 function handleButtonClicks(items) {
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,7 @@
 //Вспомогательные функции
 
 const fs = require('fs');
+const { Keyboard } = require('grammy');
 
 // Функция для обновления данных о пользователе
 function updateUserData(userDataFile, userId) {
@@ -19,4 +20,34 @@ function isAdmin(userId, adminId) {
   return userId.toString() === adminId;
 }
 
-module.exports = { updateUserData, isAdmin };
+// Функция для создания клавиатуры с кнопками и кнопкой "Назад"
+function createKeyboard(buttons) {
+  const keyboard = new Keyboard();
+  const buttonCount = buttons.length;
+  
+  // Вычисляем количество кнопок в каждой колонке
+  const buttonsPerColumn = Math.ceil(buttonCount / 2);
+  
+  for (let i = 0; i < buttonsPerColumn; i++) {
+    // Добавляем кнопку в первую колонку
+    const index1 = i;
+    keyboard.text(buttons[index1].name);
+    
+    // Проверяем, есть ли кнопка для второй колонки
+    const index2 = i + buttonsPerColumn;
+    if (index2 < buttonCount) {
+      // Добавляем кнопку во вторую колонку
+      keyboard.text(buttons[index2].name);
+    }
+    
+    // Переходим на следующую строку
+    keyboard.row();
+  }
+  
+  // Добавляем кнопку "Назад"
+  keyboard.text('Назад');
+  
+  return keyboard;
+}
+
+module.exports = { updateUserData, isAdmin, createKeyboard };
