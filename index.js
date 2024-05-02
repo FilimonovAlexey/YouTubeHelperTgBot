8a0d115280a1da5df60a83e19ea1ff0149536ac9
Alexey <alexeyf08@@gmail.com>
2024-05-02 08:59:38 +0700

    Кнопки с ссылками выведены в отдельный файл

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Bot, GrammyError, HttpError, Keyboard, InlineKeyboard } = require('grammy');
 const fs = require('fs');
 const { updateUserData, isAdmin } = require('./utils/helpers');
+const { socialNetworks, promoCodes } = require('./utils/buttons');
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
@@ -50,23 +51,6 @@ bot.command('admin', async (ctx) => {
     await ctx.reply('У вас нет прав администратора!');
   }
 });
-
-// Массив кнопок для каждой социальной сети
-const socialNetworks = [
-  { name: 'YouTube', url: 'https://www.youtube.com/@tehno.maniak' },
-  { name: 'Telegram', url: 'https://t.me/tehnomaniak07' },
-  { name: 'Vk', url: 'https://vk.com/public212223166' },
-  { name: 'ДЗЕН', url: 'https://dzen.ru/filimonov-blog.ru' },
-  { name: 'TikTok', url: 'https://www.tiktok.com/' },
-  { name: 'X', url: 'https://example.com/x' },
-  { name: 'Instagram', url: 'https://www.instagram.com/' }
-];
-
-// Массив кнопок для каждой категории промокодов и скидок
-const promoCodes = [
-  { name: 'Хостинг сервера', url: 'https://timeweb.cloud/?i=108133' },
-  { name: 'Яндекс практикум', url: 'https://example.com/yandex' },
-];
 
 // Функция для создания клавиатуры с кнопками и кнопкой "Назад"
 function createKeyboard(buttons) {
---
+++ utils/buttons.js
@@ -0,0 +1,18 @@
+// Массив кнопок для каждой социальной сети
+const socialNetworks = [
+  { name: 'YouTube', url: 'https://www.youtube.com/@tehno.maniak' },
+  { name: 'Telegram', url: 'https://t.me/tehnomaniak07' },
+  { name: 'Vk', url: 'https://vk.com/public212223166' },
+  { name: 'ДЗЕН', url: 'https://dzen.ru/filimonov-blog.ru' },
+  { name: 'TikTok', url: 'https://www.tiktok.com/' },
+  { name: 'X', url: 'https://example.com/x' },
+  { name: 'Instagram', url: 'https://www.instagram.com/' }
+];
+
+// Массив кнопок для каждой категории промокодов и скидок
+const promoCodes = [
+  { name: 'Хостинг сервера', url: 'https://timeweb.cloud/?i=108133' },
+  { name: 'Яндекс практикум', url: 'https://example.com/yandex' },
+];
+
+module.exports = { socialNetworks, promoCodes };
